3f8910533e58c4b467c1fa7dcc3f06731a7ecebe
Nunder <nathannunderwood@gmail.com>
2022-12-31 22:01:12 -0800

    update to implement Apollo server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+// no longer need RESTFUL routes since were implementing GraphQL interface
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// apply Apollo server to express as middleware
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers, 
+  context: authMiddleware, 
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +26,17 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// this is a RESTful api component. Not necessary with Apollo/GraphQL
+// app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on port ${PORT}`));
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}
+
+startApolloServer(typeDefs, resolvers);
